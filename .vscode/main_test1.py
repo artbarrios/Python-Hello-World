7abb7a2da6edafe4989939e32edc6296d5ae22f4
Art Barrios <art.barrios@outlook.com>
2020-12-06 20:44:33 -0600

    Initial commit

---
+++ .vscode/main_test1.py
@@ -0,0 +1,10 @@
+def AddTwoNumbers(x,y):
+    return x + y
+
+def main():
+    num1 = 7
+    num2 = 10
+    sum = AddTwoNumbers(num1,num2)
+    print("The sum of " + str(num1) + " and " + str(num2) + " is " + str(sum))
+
+main()
