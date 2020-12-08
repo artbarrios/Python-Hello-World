5dc1f59ecbcf6b5da57a6c8a163eac5632149186
Art Barrios <art.barrios@outlook.com>
2020-12-07 19:27:35 -0600

    Changed num2 to 13

--- .vscode/main_test1.py
+++ .vscode/main_test1.py
@@ -3,7 +3,7 @@ def AddTwoNumbers(x,y):
 
 def main():
     num1 = 7
-    num2 = 10
+    num2 = 13
     sum = AddTwoNumbers(num1,num2)
     print("The sum of " + str(num1) + " and " + str(num2) + " is " + str(sum))
 
